bc59430f9d8c3a15e6651f505a1ce2060f3b7b01
Baishalee Paul <baishalee98@gmail.com>
2020-07-25 20:15:56 +0530

    2nd commit

---
+++ products.php
@@ -0,0 +1,181 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Welcome to LifestyleStore</title>
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css">
+        <script type="text/javascript" src="bootstrap/js/jquery-3.4.1.min.js"></script>
+        <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="style.css" type="text/css">
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+        <header>
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a href="index.php" class="navbar-brand">Lifestyle Store</a>
+                </div>
+                <div class="collapse navbar-collapse" id="myNavbar">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="cart.php"><span class="glyphicon glyphicon-shopping-cart"></span>Cart</a></li>
+                        <li><a href="settings.php"><span class="glyphicon glyphicon-user"></span>Settings</a></li>
+                        <li><a href="#"><span class="glyphicon glyphicon-log-out"></span>Logout</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        </header>
+        
+        <div class="container">
+            <div class="jumbotron">
+                <h1>Welcome to our Lifestyle Store!</h1>
+                <p> We have the best cameras, watches and shirts for you. No need to hunt around, we have all in one place.</p>
+            </div>
+            <div class="row text-center">
+                <div class="col-md-3 col-sm-6">
+                    <div class="thumbnail">
+                        <img src="img/5.jpg" alt="Cannon EOS">
+                        <div class="caption">
+                            <h4 style="font-weight: bold">Cannon EOS</h4>
+                            <p>Price Rs. 36000.00</p>
+                            <button class="btn btn-primary btn-block">Add to cart</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6">
+                    <div class="thumbnail">
+                        <img src="img/2.jpg" alt="Sony DSLR">
+                        <div class="caption">
+                            <h4 style="font-weight: bold">Sony DSLR</h4>
+                            <p>Price Rs. 40000.00</p>
+                            <button class="btn btn-primary btn-block">Add to cart</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6">
+                    <div class="thumbnail">
+                        <img src="img/3.jpg" alt="Sony DSLR">
+                        <div class="caption">
+                            <h4 style="font-weight: bold">Sony DSLR</h4>
+                            <p>Price Rs. 50000.00</p>
+                            <button class="btn btn-primary btn-block">Add to cart</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6">
+                    <div class="thumbnail">
+                        <img src="img/4.jpg" alt="Olympus DSLR">
+                        <div class="caption">
+                            <h4 style="font-weight: bold">Olympus DSLR</h4>
+                            <p>Price Rs. 60000.00</p>
+                            <button class="btn btn-primary btn-block">Add to cart</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row text-center">
+                <div class="col-md-3 col-sm-6">
+                    <div class="thumbnail">
+                        <img src="img/9.jpg" alt="Titan Model #301">
+                        <div class="caption">
+                            <h4 style="font-weight: bold">Titan Model #301</h4>
+                            <p>Price Rs. 13000.00</p>
+                            <button class="btn btn-primary btn-block">Add to cart</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6">
+                    <div class="thumbnail">
+                        <img src="img/10.jpg" alt="Titan Model #201">
+                        <div class="caption">
+                            <h4 style="font-weight: bold">Titan Model #201</h4>
+                            <p>Price Rs. 3000.00</p>
+                            <button class="btn btn-primary btn-block">Add to cart</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6">
+                    <div class="thumbnail">
+                        <img src="img/11.jpg" alt="HMT Milan">
+                        <div class="caption">
+                            <h4 style="font-weight: bold">HMT Milan</h4>
+                            <p>Price Rs. 8000.00</p>
+                            <button class="btn btn-primary btn-block">Add to cart</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6">
+                    <div class="thumbnail">
+                        <img src="img/12.jpg" alt="Faber Luba #111">
+                        <div class="caption">
+                            <h4 style="font-weight: bold">Faber Luba #111</h4>
+                            <p>Price Rs. 18000.00</p>
+                            <button class="btn btn-primary btn-block">Add to cart</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row text-center">
+                <div class="col-md-3 col-sm-6">
+                    <div class="thumbnail">
+                    <img src="img/8.jpg" alt="H&W">
+                    <div class="caption">
+                        <h4 style="font-weight: bold">H&W</h4>
+                        <p>Price Rs. 800.00</p>
+                        <button class="btn btn-primary btn-block">Add to cart</button>
+                    </div>
+                    </div>
+                </div>
+                    
+                <div class="col-md-3 col-sm-6">
+                    <div class="thumbnail">
+                        <img src="img/6.jpg" alt="Luis Phil">
+                        <div class="caption">
+                            <h4 style="font-weight: bold">Luis Phil</h4>
+                            <p>Price Rs. 1000.00</p>
+                            <button class="btn btn-primary btn-block">Add to cart</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6">
+                    <div class="thumbnail">
+                        <img src="img/13.jpg" alt="John Zok">
+                        <div class="caption">
+                            <h4 style="font-weight: bold">John Zok</h4>
+                            <p>Price Rs. 1500.00</p>
+                            <button class="btn btn-primary btn-block">Add to cart</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6">
+                    <div class="thumbnail">
+                        <img src="img/14.jpg" alt="Jhalsani">
+                        <div class="caption">
+                            <h4 style="font-weight: bold">Jhalsani</h4>
+                            <p>Price Rs. 1300.00</p>
+                            <button class="btn btn-primary btn-block">Add to cart</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <footer>
+            <div class="container">
+                <center>
+                    <p>Copyright © Lifestyle Store. All Rights Reserved | Contact Us: +91 90000 00000</p>
+                </center>
+            </div>
+        </footer>
+        
+        
+    </body>
+</html>
+
